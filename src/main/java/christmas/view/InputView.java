695a8d62ad1fc023c32dcbd0533651c4cfbcea49
rednada1486 <109245648+rednada1486@users.noreply.github.com>
2023-11-12 12:25:02 +0900

    [#1 Task2] feat(InputView): 고객으로 부터 주문을 입력받는다
    - readOrderList 메서드는 고객으로 부터 주문을 입력받는 메서드 입니다.
    - 추가로 InputMessage 상숫값을 담아둘 enum도 만들었습니다.

---
+++ src/main/java/christmas/view/InputView.java
@@ -0,0 +1,11 @@
+package christmas.view;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+import static christmas.view.InputMessage.WHAT_WOULD_YOU_LIKE_TO_ORDER;
+
+public class InputView {
+    public static String readOrderList() {
+        System.out.println(WHAT_WOULD_YOU_LIKE_TO_ORDER.getInputMessage());
+        return readLine();
+    }
+}
