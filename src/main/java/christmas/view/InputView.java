6bc0756c25ad8dbddc62c2ff6ccd685a8abe2800
rednada1486 <109245648+rednada1486@users.noreply.github.com>
2023-11-13 08:06:43 +0900

    [#1 Task6] test(ReservationController): 입력받은 주문을 주문리스트로 만든다
    - receiveOrder() 메서드가 잘 동작하는지 확인하는 테스트 코드를 작성하였습니다.
    - 테스트 도중 출력 문구가 어색하여 수정하였습니다. (WHAT_WOULD_YOU_LIKE_TO_ORDER)
    - 입력값을 받을 때 메뉴판을 보여줄 수 있도록 printAllMenu() 메서드의 위치를 선정하였습니다.

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -2,10 +2,13 @@ package christmas.view;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 import static christmas.view.InputMessage.WHAT_WOULD_YOU_LIKE_TO_ORDER;
+import static christmas.view.OutputView.*;
 
 public class InputView {
     public static String readOrderList() {
         System.out.println(WHAT_WOULD_YOU_LIKE_TO_ORDER.getInputMessage());
+        System.out.println();
+        printAllMenu();
         return readLine();
     }
 }
---
+++ src/test/java/christmas/controller/ReservationControllerTest.java
@@ -0,0 +1,54 @@
+package christmas.controller;
+
+import camp.nextstep.edu.missionutils.Console;
+import christmas.domain.Menu;
+import christmas.domain.Order;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.List;
+
+import static christmas.domain.Menu.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReservationControllerTest {
+    private ReservationController reservationController;
+
+    InputStream createUserInput(String input) {
+        return new ByteArrayInputStream(input.getBytes());
+    }
+
+
+    @BeforeEach
+    public void beforeEach() {
+        reservationController = new ReservationController();
+    }
+
+    @AfterEach
+    public void afterEach() {
+        Console.close();
+    }
+
+    @Test
+    @DisplayName("receiveOrder 메서드는 유저 입력값을 변화하여 주문 목록을 반환한다.")
+    void receiveOrderShouldReturnOrderList() {
+        // given
+        System.setIn(createUserInput("타파스-1,제로콜라-1"));
+
+        // when
+        List<Order> result = reservationController.receiveOrder();
+
+        // then
+        assertThat(result).hasSize(2);
+        assertThat(result.get(0).getMenu()).isEqualTo(TAPAS);
+        assertThat(result.get(0).getCount()).isEqualTo(1);
+        assertThat(result.get(1).getMenu()).isEqualTo(ZERO_COLA);
+        assertThat(result.get(1).getCount()).isEqualTo(1);
+    }
+}
