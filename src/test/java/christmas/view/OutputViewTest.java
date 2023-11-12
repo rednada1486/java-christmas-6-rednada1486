2a70acc40506f08d127725bcd52f45b926cddb3c
rednada1486 <109245648+rednada1486@users.noreply.github.com>
2023-11-13 07:35:38 +0900

    [#1 Task5] test(OutputView): 주문 리스트를 유저에게 출력한다
    - printOrderList() 메서드가 잘 동작하는지 확인하는 테스트 코드를 작성하였습니다.

--- src/test/java/christmas/view/OutputViewTest.java
+++ src/test/java/christmas/view/OutputViewTest.java
@@ -1,12 +1,18 @@
 package christmas.view;
 
+import christmas.domain.Menu;
+import christmas.domain.Order;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.mockito.internal.matchers.Or;
+
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import static christmas.domain.Menu.*;
+import static org.assertj.core.api.Assertions.*;
 
 
 class OutputViewTest {
@@ -31,6 +37,22 @@ class OutputViewTest {
         String result = outputStreamCaptor.toString();
 
         // then
-        Assertions.assertThat(result).containsSubsequence("<애피타이저>", "<메인>", "<디저트>", "<음료>");
+        assertThat(result).containsSubsequence("<애피타이저>", "<메인>", "<디저트>", "<음료>");
+    }
+
+    @Test
+    @DisplayName("주문 목록을 원하는 형식에 맞춰서 출력한다.")
+    void printOrderList() {
+        // given
+        List<Order> orderList = new ArrayList<>();
+        orderList.add(new Order(ZERO_COLA, 1));
+        orderList.add(new Order(TAPAS, 2));
+
+        // when
+        OutputView.printOrderList(orderList);
+        String result = outputStreamCaptor.toString();
+
+        // then
+        assertThat(result).contains("제로콜라 1개", "타파스 2개");
     }
 }
