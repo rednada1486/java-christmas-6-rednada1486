b02fcfdf6e38593114da4cf9e7adb4d717c07764
rednada1486 <109245648+rednada1486@users.noreply.github.com>
2023-11-13 05:32:57 +0900

    [#1 Task4] test(domain.Menu): Menu enum 검색 기능 구현
    - findByName() 메서드가 잘 동작하는지 확인하는 테스트 코드를 작성하였습니다.

---
+++ src/test/java/christmas/domain/MenuTest.java
@@ -0,0 +1,35 @@
+package christmas.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MenuTest {
+
+    @Test
+    @DisplayName("findByName 메서드에 메뉴판에 있는 메뉴 이름을 입력하면 Menu enum을 돌려준다.")
+    void findByNameShouldReturnMenuEnumWhenNameContainsMenuList() {
+        // given
+        String menuName = "제로콜라";
+
+        // when
+        Menu menu = Menu.findByName(menuName);
+
+        // then
+        assertThat(menu).isEqualTo(Menu.ZERO_COLA);
+    }
+
+    @Test
+    @DisplayName("findByName 메서드에 메뉴판에 없는 이름을 검색하면 null을 반환한다.")
+    void findByNameShouldReturnMenuEnum() {
+        // given
+        String menuName = "없는 메뉴";
+
+        // when
+        Menu menu = Menu.findByName(menuName);
+
+        // then
+        assertThat(menu).isEqualTo(null);
+    }
+}
