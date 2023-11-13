6225c7ccf2b2ae8d5c9e1dccb03e336437a2e42f
rednada1486 <109245648+rednada1486@users.noreply.github.com>
2023-11-13 15:31:39 +0900

    [#3 Task4] test(domain.Order): 메뉴의 개수가 1보다 작은 경우 예외를 발생시킨다
    - 유저 입력값으로 Order 객체를 생성할 때, 메뉴의 개수가 1보다 작은 경우 예외를 발생시키는지 확인하는 테스트 코드를 작성했습니다.

--- src/test/java/christmas/domain/OrderTest.java
+++ src/test/java/christmas/domain/OrderTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 
 class OrderTest {
     @Test
-    @DisplayName("정상 입력값으로(메뉴-숫자) Order 객체를 생성하면 예외를 발생시키지 않는다.")
+    @DisplayName("유저입력값으로 Order 객체를 생성할 때, 정상 입력값일 경우 예외를 발생시킨다.")
     void orderGeneratorNotThrowIllegalArgumentExceptionWhenNumberIsLagerThanZero() {
         // given
         String userInput = "제로콜라-1";
@@ -19,7 +19,7 @@ class OrderTest {
     }
 
     @Test
-    @DisplayName("문자-숫자가 형태가 아닌 문자열로 Order 객체를 생성하면 예외를 발생시킨다.")
+    @DisplayName("유저입력값으로 Order 객체를 생성할 때, 유저입력값이 문자-숫자가 형태가 아니라면 예외를 발생시킨다.")
     void orderGeneratorThrowIllegalArgumentExceptionWhenArgumentFormatIsNotStringHyphenNumber() {
         // given
         String[] userInputArr = {"-", "제로콜라-", "-1", "제로콜라>1", "제로콜라->1", "제로콜라=1", "제로콜라,1", "제로-콜라1"};
@@ -33,10 +33,22 @@ class OrderTest {
     }
 
     @Test
-    @DisplayName("존재하지 않는 메뉴로 Order 객체를 생성하면 예외를 발생시킨다.")
+    @DisplayName("유저입력값으로 Order 객체를 생성할 때, 유저입력값의 메뉴가 존재하지 않는 메뉴라면 예외를 발생시킨다.")
     void orderGeneratorThrowIllegalArgumentExceptionWhenUnavailableMenuIsInputted() {
         // given
         String userInput = "제로콜리-1";
+
+        // when, then
+        assertThatThrownBy(() -> new Order(userInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INVALID_ORDER_MESSAGE.getErrorMessage());
+    }
+
+    @Test
+    @DisplayName("유저입력값으로 Order 객체를 생성할 때, 유저입력값의 숫자가 1보다 작은 경우 예외를 발생시킨다.")
+    void orderGeneratorThrowIllegalArgumentExceptionWhenCountIsSmallerThanOne() {
+        // given
+        String userInput = "제로콜라-0";
 
         // when, then
         assertThatThrownBy(() -> new Order(userInput))
