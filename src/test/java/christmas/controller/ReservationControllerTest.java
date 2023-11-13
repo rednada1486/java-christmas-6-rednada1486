1387dbf7fb9367d1ba0f2d09d349d3fa8cca875d
rednada1486 <109245648+rednada1486@users.noreply.github.com>
2023-11-13 18:32:39 +0900

    [#3 Task8] test(ReservationController): 사용자가 잘못된 주문을 입력할 경우 정말로 예외를 발생시키는지 확인한다
    - 사용자가 잘못된 주문을 할 경우 정말로 예외를 발생시키는지 확인하는 테스트 코드를 작성했습니다.
    - 테스트 케이스
    "타파스->1" : 잘못된 형식으로 주문 입력
    "없는메뉴-1" : 없는 메뉴 입력
    "타파스-0" : 주문 메뉴의 개수를 1보다 작게 입력
    "타파스-1,타파스-1" : 중복 메뉴 입력
    "타파스-21" : 주문한 메뉴의 전체 개수가 20초과
    "제로콜라-1" : 음료만 주문

--- src/test/java/christmas/controller/ReservationControllerTest.java
+++ src/test/java/christmas/controller/ReservationControllerTest.java
@@ -1,6 +1,7 @@
 package christmas.controller;
 
 import static christmas.domain.Menu.*;
+import static christmas.view.ErrorMessage.*;
 import static org.assertj.core.api.Assertions.*;
 
 import camp.nextstep.edu.missionutils.Console;
@@ -9,10 +10,11 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.List;
-
 
 class ReservationControllerTest {
     private ReservationController reservationController;
@@ -20,7 +22,6 @@ class ReservationControllerTest {
     InputStream createUserInput(String input) {
         return new ByteArrayInputStream(input.getBytes());
     }
-
 
     @BeforeEach
     public void beforeEach() {
@@ -33,7 +34,7 @@ class ReservationControllerTest {
     }
 
     @Test
-    @DisplayName("receiveOrder 메서드는 유저 입력값을 변화하여 주문 목록을 반환한다.")
+    @DisplayName("receiveOrder 메서드는 유저 입력값을 변환하여 주문 목록을 반환한다.")
     void receiveOrderShouldReturnOrderList() {
         // given
         System.setIn(createUserInput("타파스-1,제로콜라-1"));
@@ -47,5 +48,18 @@ class ReservationControllerTest {
         assertThat(result.get(0).getCount()).isEqualTo(1);
         assertThat(result.get(1).getMenu()).isEqualTo(ZERO_COLA);
         assertThat(result.get(1).getCount()).isEqualTo(1);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"타파스->1", "없는메뉴-1", "타파스-0", "타파스-1,타파스-1", "타파스-20,제로콜라-1", "제로콜라-1"})
+    @DisplayName("receiveOrder 메서드는 유저가 잘못된 값을 입력할 경우 예외를 발생시킨다.")
+    void receiveOrderShouldThrowIllegalArgumentExceptionWhenWrongValueInputted(String input) {
+        // given
+        System.setIn(createUserInput(input));
+
+        // when, then
+        assertThatThrownBy(() -> reservationController.receiveOrder())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INVALID_ORDER_MESSAGE.getErrorMessage());
     }
 }
