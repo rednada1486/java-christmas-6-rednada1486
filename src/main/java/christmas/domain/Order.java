39f17c2ad70958bcadeea71832c788dfba4d727d
rednada1486 <109245648+rednada1486@users.noreply.github.com>
2023-11-13 14:46:46 +0900

    [#3 Task2~4] feat(domain.Order): 주문을 위해 유저가 입력한 값을 검증한다
    - validate() 메서드는 전반적으로 유저 입력값을 검증하고 검증에 통과하지 못하면 예외를 발생시키는 메서드 입니다.
    - checkMenuHyphenCount() 메서드는 유저 입력값의 형식이 "문자-숫자" 형태인지 정규표현식을 활용하여 확인하는 메서드 입니다.
    - checkMenu() 메서드는 메뉴판에 있는 메뉴인지 확인하는 메서드 입니다.
    - checkCount() 메서드는 숫자 인지 확인하고 숫자라면 1보다 크거나 같은 값인지 확인하는 메서드 입니다.

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -1,5 +1,9 @@
 package christmas.domain;
 
+import static christmas.view.ErrorMessage.*;
+
+import java.util.regex.Pattern;
+
 public class Order {
     private Menu menu;
     private int count;
@@ -10,11 +14,12 @@ public class Order {
     }
 
     public Order(String menuHyphenCount) {
-        String menuName = menuHyphenCount.split("-")[0];
-        String count = menuHyphenCount.split("-")[1];
+        validate(menuHyphenCount);
 
-        this.menu = Menu.findByName(menuName);
-        this.count = Integer.parseInt(count);
+        String[] splitInput = menuHyphenCount.split("(?=-\\d+$)");
+
+        this.menu = Menu.findByName(splitInput[0]);
+        this.count = Integer.parseInt(splitInput[1]);
     }
 
     public Menu getMenu() {
@@ -36,6 +41,42 @@ public class Order {
     public int calculateSubTotal() {
         return menu.getPrice() * count;
     }
+
+    private void validate(String menuHyphenCount) {
+        if (checkMenuHyphenCount(menuHyphenCount)){
+            throw new IllegalArgumentException(INVALID_ORDER_MESSAGE.getErrorMessage());
+        }
+
+        String[] splitInput = menuHyphenCount.split("(?=-\\d+$)");
+
+        if (!checkMenu(splitInput[0]) || !checkCount(splitInput[1])) {
+            throw new IllegalArgumentException(INVALID_ORDER_MESSAGE.getErrorMessage());
+        }
+    }
+
+    private boolean checkMenuHyphenCount(String menuHyphenCount) {
+        String pattern = "^.+-\\d+";
+        return Pattern.matches(pattern, menuHyphenCount);
+    }
+
+    private boolean checkMenu(String menuName) {
+        return Menu.findByName(menuName) != null;
+    }
+
+    private boolean checkCount(String countStr) {
+        int count = 0;
+
+        try {
+            count = Integer.parseInt(countStr);
+        } catch (Exception e) {
+            return false;
+        }
+
+        return count >= 1;
+    }
+
+
+
 
     @Override
     public String toString() {
