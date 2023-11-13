e0bed087ea623e7090c19937eb835dbaaa020406
rednada1486 <109245648+rednada1486@users.noreply.github.com>
2023-11-13 14:21:02 +0900

    [#3 Task1] feat(view.ErrorMessage): 에러메시지를 모아 놓은 Enum을 생성한다
    - ErrorMessage 는 에러메시지를 모아놓은 열거형 클래스 입니다.

---
+++ src/main/java/christmas/view/ErrorMessage.java
@@ -0,0 +1,15 @@
+package christmas.view;
+
+public enum ErrorMessage {
+    INVALID_ORDER_MESSAGE("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+
+    private final String errorMessage;
+
+    ErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+}
