72093e9e957276ecae5ac5ae15ea4b4a2dc53794
rednada1486 <109245648+rednada1486@users.noreply.github.com>
2023-11-14 10:05:05 +0900

    [#5 Task6] feat(InputView): 유저에게 날짜를 입력받는다
    - readReservationDate() 메서드는 유저로 부터 예약날짜를 입력 받는 메서드 입니다.

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -2,12 +2,18 @@ package christmas.view;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 import static christmas.view.InputMessage.WHAT_WOULD_YOU_LIKE_TO_ORDER;
+import static christmas.view.InputMessage.WHEN_IS_THE_EXPECTED_VISIT_DAY_IN_DECEMBER;
 import static christmas.view.OutputView.*;
 
 public class InputView {
     public static String readOrderList() {
         System.out.println(WHAT_WOULD_YOU_LIKE_TO_ORDER.getInputMessage());
         printAllMenu();
+        return readLine();
+    }
+
+    public static String readReservationDate() {
+        System.out.println(WHEN_IS_THE_EXPECTED_VISIT_DAY_IN_DECEMBER.getInputMessage());
         return readLine();
     }
 }
