4fbfc1432e7b36ae32fe56d5f4aa564e9c7be66d
rednada1486 <109245648+rednada1486@users.noreply.github.com>
2023-11-15 09:44:56 +0900

    [#9 Task2] feat(Domain.Bill) : 할인 전 총 주문 금액을 계산한다
    - calculateOriginalPaymentAmount() 메서드는 주문 목록을 사용해서 결제 전 총 주문 금액을 계산하는 메서드 입니다.

--- src/main/java/christmas/domain/Bill.java
+++ src/main/java/christmas/domain/Bill.java
@@ -8,10 +8,11 @@ public class Bill {
     private final BenefitCalculator benefitCalculator = new BenefitCalculator();
 
     private final List<Order> orderList;
-
+    private final int originalPaymentAmount;
 
     public Bill(Date date, List<Order> orderList) {
         this.orderList = orderList;
+        originalPaymentAmount = calculateOriginalPaymentAmount(orderList);
     }
 
     private int calculateOriginalPaymentAmount(List<Order> orderList) {
@@ -22,5 +23,9 @@ public class Bill {
 
     public List<Order> getOrderList() {
         return orderList;
+    }
+
+    public int getOriginalPaymentAmount() {
+        return originalPaymentAmount;
     }
 }
